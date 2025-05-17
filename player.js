f793d11480c993acaba7c381084f249222d9ee65
Andrea Lucero <alucerotrasvina@gmail.com>
2025-05-16 17:06:58 -0700

    Jugador

---
+++ player.js
@@ -0,0 +1,15 @@
+class Player{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.width = 20;
+        this.height = 20;
+        this.speed = 1;
+        this.lives = 3;
+    }
+
+    dibujar(){
+        fill(255, 255, 0); 
+        rect(this.x, this.y, this.width, this.height);
+    }
+}
--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+let player;
+
 function setup() {
     createCanvas(500, 600);
+    player = new Player(width / 2, height / 2);
   }
   
   function draw() {
     background(255);
+    player.dibujar();
   }
