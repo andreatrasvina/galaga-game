e457a7912ccb4aa4a59d167ec39959d2c3116555
HazaFG <hazaelfg@gmail.com>
2025-05-16 17:21:31 -0700

    movimiento del mono

--- player.js
+++ player.js
@@ -1,15 +1,25 @@
-class Player{
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
-        this.width = 20;
-        this.height = 20;
-        this.speed = 1;
-        this.lives = 3;
-    }
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 20;
+    this.height = 20;
+    this.speed = 1;
+    this.lives = 3;
+  }
 
-    dibujar(){
-        fill(255, 255, 0); 
-        rect(this.x, this.y, this.width, this.height);
+  dibujar() {
+    fill(255, 255, 0);
+    rect(this.x, this.y, this.width, this.height);
+  }
+
+  mover() {
+    if (keyIsPressed) {
+      if (keyCode == RIGHT_ARROW) {
+        this.x += 5;
+      } else if (keyCode == LEFT_ARROW) {
+        this.x -= 5;
+      }
     }
+  }
 }
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let player;
 
 function setup() {
-    createCanvas(500, 600);
-    player = new Player(width / 2, height / 2);
-  }
-  
-  function draw() {
-    background(255);
-    player.dibujar();
-  }
+  createCanvas(500, 600);
+  player = new Player(width = 240, height = 530);
+}
+
+function draw() {
+  background(255);
+  player.dibujar();
+  player.mover()
+}
