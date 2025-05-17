506afa0fcc388799a72fbbcf34e646083ba6fff4
Andrea Lucero <alucerotrasvina@gmail.com>
2025-05-16 20:34:34 -0700

    Fondo del juego

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 let player;
 let bullets = [];
+let fondo;
+let yFondo = -20;
+
+function preload() {
+    fondo = loadImage('assets/images/bg-game.png'); 
+}
+
 
 function setup() {
-  createCanvas(500, 600);
-  player = new Player(width = 240, height = 530);
+    createCanvas(500, 600);
+    player = new Player(240, 530);
 }
 
 function draw() {
-  background(255);
-  player.dibujar();
-  player.mover();
+    background(0);
 
-  //muestra balas del array bullets
-  for (let i = bullets.length - 1; i >= 0; i--) {
-    bullets[i].mover();
-    bullets[i].dibujar();
+    //movimiento fondo
+    yFondo += 2;
 
-    //elimina las que salen de la pantalla
-    if (bullets[i].desaparece()) {
-      bullets.splice(i, 1);
+    //que la foto se repita verticalmente
+    image(fondo, 0, yFondo % height - height, width, height);
+    image(fondo, 0, yFondo % height, width, height);
+    
+
+    //juego
+    player.dibujar();
+    player.mover();
+
+    //muestra balas del array bullets
+    for (let i = bullets.length - 1; i >= 0; i--) {
+        bullets[i].mover();
+        bullets[i].dibujar();
+
+        //elimina las que salen de la pantalla
+        if (bullets[i].desaparece()) {
+        bullets.splice(i, 1);
+        }
     }
-  }
 }
 
 //espacio para añadir balas al array
